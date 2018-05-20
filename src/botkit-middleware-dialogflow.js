962d91e0d51306f95bf121dee3b46b4313f1473d
Luca <lomluca12@gmail.com>
2018-05-20 18:16:34 +0200

    Fix check undefined of message.language

--- src/botkit-middleware-dialogflow.js
+++ src/botkit-middleware-dialogflow.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
             }
         }
         
-        if(message.language == null) {
+        if(message.language != null) {
             app.language = message.language;
         }
 
